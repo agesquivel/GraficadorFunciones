354454fd6cff00872a8ea8af3613de66798b199a
lab <lab.computo.itch@gmail.com>
2016-10-15 12:26:54 -0500

    Initial commit

---
+++ app/src/main/java/itchetumal/edu/mx/graficadorfunciones/MainActivity.java
@@ -0,0 +1,133 @@
+package itchetumal.edu.mx.graficadorfunciones;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+
+import android.graphics.Rect;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        LinearLayout layout1 = (LinearLayout) findViewById(R.id.LayoutPrincipal);
+        Lienzo fondo = new Lienzo(this);
+        layout1.addView(fondo);
+    }
+
+    class Lienzo extends View {
+
+        public Lienzo(Context context) {
+            super(context);
+        }
+
+        protected void onDraw(Canvas canvas) {
+            //super.onDraw(canvas);
+            //int ancho = canvas.getWidth();
+            //int alto = canvas.getHeight();
+            //int ancho = this.getRight();
+            //int alto = this.getBottom();
+
+            int ancho = this.getMeasuredWidth();
+            int alto = this.getMeasuredHeight();
+
+            Paint pincel1 = new Paint();
+
+            pincel1.setARGB(255, 0, 0, 0);
+            pincel1.setStyle(Paint.Style.STROKE);
+            canvas.drawPoint(ancho / 2, alto / 2, pincel1);
+            canvas.drawRect(0, 0, ancho-1, alto-1, pincel1);
+
+            pincel1.setARGB(255, 255, 0, 0);
+            canvas.drawRect(ancho/2 - 100, alto/2 - 100, ancho/2+100, alto/2+100, pincel1);
+
+            pincel1.setColor(Color.BLUE);
+            pincel1.setStrokeWidth(3);
+
+            //dibujar linea de eje X
+            canvas.drawLine(0, alto/2, ancho - 1, alto/2, pincel1);
+
+            //dibujar linea de eje XY
+            canvas.drawLine(ancho/2, 0, ancho/2, alto-1, pincel1);
+
+            //Dibujar texto
+            pincel1.setARGB(255, 100, 100, 0);
+            pincel1.setTextSize(30);
+            pincel1.setStrokeWidth(3);
+            //canvas.drawText("Alto: " + alto, ancho/2, alto/2, pincel1);
+            canvas.drawText("Graficador", ancho/2, alto/2, pincel1);
+
+            pincel1.setColor(Color.RED);
+            String texto = "Graficador";
+
+            Rect limites = new Rect();
+            pincel1.getTextBounds(texto, 0, texto.length(), limites);
+
+            limites.set(ancho/2 + limites.left, alto/2 + limites.top,
+                    ancho/2 + limites.right, alto/2 + limites.bottom);
+
+            canvas.drawRect(limites, pincel1);
+
+            float centroX = limites.exactCenterX();
+            float centroY = limites.exactCenterY();
+
+            //Rotar texto a 90 grados
+            /*
+            String textY = "EjeY";
+
+            Rect limTxtY = new Rect();
+            pincel1.getTextBounds(textY, 0, textY.length(), limTxtY);
+
+            canvas.rotate(-90, ancho/2, alto/2);
+
+            int anchoRot = canvas.getHeight();
+            int altoRot = canvas.getWidth();
+
+            canvas.drawText("Origen canvas rotado", anchoRot, altoRot, pincel1);
+
+            canvas.drawText("Eje Y", altoRot/2, anchoRot/2, pincel1);
+            //canvas.restore();
+
+            canvas.rotate(90, ancho/2, alto/2);
+            pincel1.setColor(Color.BLACK);
+            pincel1.setStrokeWidth(2);
+
+            pincel1.setStyle(Paint.Style.FILL);
+            pincel1.setTextSize(20);
+            canvas.drawText("Límites: " + limTxtY.toShortString(), 0, alto - 10, pincel1);
+            canvas.drawText("Nuevos valores ancho: " + anchoRot
+                             + " alto: " + altoRot, 0, alto - 30, pincel1);
+
+           */
+
+            double gradRad = 0;
+            double senGrado = 0;
+
+            //Variables para transformación de coordenadas
+            int coordX, coordY;
+
+            for (int grado = 0; grado <= 360; grado++){
+                gradRad = grado * 3.14/180;
+                senGrado = Math.sin(gradRad);
+
+                //Cálculos de coordenadas
+
+
+                //Dibujado de puntos
+                canvas.drawPoint(coordX, coordY, pincel1);
+            }
+
+
+
+        }
+    }
+}
